aff1b291365dc7d9d24d181dba0956390af1ee07
John Clay <johnsclay45@gmail.com>
2017-04-11 15:48:50 -0500

    first code commit

---
+++ Eclipse Test/RepositoryTest/src/Addition/Add.java
@@ -0,0 +1,9 @@
+package Addition;
+
+public class Add 
+{
+	public int addNumbers(int x, int y)
+	{
+		return x + y;
+	}
+}
---
+++ Eclipse Test/RepositoryTest/src/Print/Print.java
@@ -0,0 +1,13 @@
+package Print;
+
+import Addition.Add;
+
+public class Print {
+
+	public static void main(String[] args) 
+	{
+		Add adder = new Add();
+		System.out.println(adder.addNumbers(3, 8));
+	}
+
+}
